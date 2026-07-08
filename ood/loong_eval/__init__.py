99c95f49130abbdd1eaadc2be0abc7a4c8e8d040
oaimli <miaoli.cs@gmail.com>
2026-07-08 17:58:00 +0100

    update

--- ood/loong_eval/__init__.py
+++ ood/loong_eval/__init__.py
@@ -1,6 +1,5 @@
-# Loong evaluation
-# follow the instructions in README.md
+# Loong evaluation, following the instructions in README.md
 # for each sample, the generation results are put under the key of "generations" in the model output file
-# the final scores are accessible in aggregated.jsonl
 
-# e.g., ./run_evaluation.sh ../loong_qwen25_7b_instruct_1m_scitrek_grpo_test.jsonl {the-project-path}/train_other/gpt5_eval gpt5-mini.yaml
+# e.g., ./run_evaluation.sh ../loong_gemma3_4b_it_scitrek_500_test.jsonl {the-project-path}/ood/loong_gemma3_4b_it_scitrek_500_test gpt5-mini.yaml
+# the final scores are saved in loong_gemma3_4b_it_scitrek_500_test/aggregated.jsonl
