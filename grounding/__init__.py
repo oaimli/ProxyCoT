20278f582a1c2599576143b6b457e0d02ad53119
oaimli <miaoli.cs@gmail.com>
2026-07-08 17:13:22 +0100

    update

--- grounding/__init__.py
+++ grounding/__init__.py
@@ -3,7 +3,7 @@
 # direct-grounding (ProxyCoT-ZS):
 # training the initial model on full contexts, with the reasoning traces from the teacher model based on proxy contexts
 
-# reinforcement_0:
+# reinforcement_0 (ablations):
 # get the reasoning traces from the RL based model on proxy contexts
 # use the reasoning traces to train the initial model on full contexts
 
--- testing/__init__.py
+++ testing/__init__.py
@@ -1,1 +1,2 @@
 # testing zero-short prompting with popular long-context models
+# generation + evaluation
